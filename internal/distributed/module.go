f9d8c45bf89544ed5bc2bc034fbb5a3fd109824f
ddddd <dai_yuang@icloud.com>
2026-01-20 10:29:23 +0800

    style(distributed): 格式化代码缩进和空格
    - 统一使用 tab 缩进替换混合空格缩进
    - 修复 import 语句间的空行格式
    - 格式化函数体内的代码缩进一致性
    - 标准化括号和语句间的空行格式
    - 规范化变量声明和返回语句的格式
    - 保持代码风格与项目规范一致

--- internal/distributed/module.go
+++ internal/distributed/module.go
@@ -1,26 +1,26 @@
 package distributed
 
 import (
-  "log/slog"
+	"log/slog"
 
-  "github.com/hashicorp/go-uuid"
-  "go.uber.org/fx"
+	"github.com/hashicorp/go-uuid"
+	"go.uber.org/fx"
 )
 
 var Module = fx.Module("distributed", fx.Provide(
-  func(logger *slog.Logger) *Store {
-    store := New(true, logger)
-    store.RaftBind = "localhost:12000"
-    return store
-  },
+	func(logger *slog.Logger) *Store {
+		store := New(true, logger)
+		store.RaftBind = "localhost:12000"
+		return store
+	},
 ),
-  fx.Invoke(open),
+	fx.Invoke(open),
 )
 
 func open(s *Store) error {
-  generateUUID, err := uuid.GenerateUUID()
-  if err != nil {
-    return err
-  }
-  return s.Open(true, generateUUID)
+	generateUUID, err := uuid.GenerateUUID()
+	if err != nil {
+		return err
+	}
+	return s.Open(true, generateUUID)
 }
